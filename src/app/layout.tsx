9778376806385a42e6615f7ea43ee0308fc3baa7
Quan PM <quan.phamminh@hdwebsoft.dev>
2025-05-21 23:47:50 +0700

    feat: add progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/layout/common/ThemeProvider";
+import RouteProgress from "@/components/layout/common/RouteProgress";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
         cz-shortcut-listen="true"
       >
         <Providers>
+          <RouteProgress />
           {children}
         </Providers>
       </body>
